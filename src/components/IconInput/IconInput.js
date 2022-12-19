7c3a0cc1abb6236dc4391791eabd3e867cf46fb9
Diederik van Remoortere <diederik.van.remoortere@gmail.com>
2022-12-20 00:06:33 +0100

    IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
+
+const STYLES = {
+  small: {
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24,
+  },
+  large: {
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36,
+  },
+};
 
 const IconInput = ({
   label,
@@ -12,8 +27,67 @@ const IconInput = ({
   width = 250,
   size,
   placeholder,
+  ...delegated
 }) => {
-  return 'TODO';
+  const styles = STYLES[size];
+  if (!styles) {
+    // In typescript this would be handled by props ;)
+    throw new Error(`Unkown size passed to ProgessBar: ${size}`);
+  }
+  return (
+    <Wrapper style={{}}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{ "--size": styles.iconSize + "px" }}>
+        <Icon id={icon} size={styles.iconSize} />
+      </IconWrapper>
+
+      <TextInput
+        placeholder={placeholder}
+        {...delegated}
+        style={{
+          "--font-size": styles.fontSize + "px",
+          "--width": width + "px",
+          "--height": styles.height + "px",
+          "--borderThickness": styles.borderThickness + "px",
+        }}
+      />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+  /* left: 4px; */
+`;
+
+const TextInput = styled.input`
+  width: var(--width);
+  height: var(--height);
+  font-size: var(--font-size);
+  border: none;
+  border-bottom: var(--borderThickness) solid ${COLORS.black};
+  padding-left: var(--height);
+  color: inherit;
+  font-weight: 700;
+  outline-offset: 2px;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`;
 
 export default IconInput;
